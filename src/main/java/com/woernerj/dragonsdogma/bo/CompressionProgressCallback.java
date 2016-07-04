ba5621a6adc89928f9367ef1f228d9d9267868d2
Jordan Woerner <jordanw2004@googlemail.com>
2016-07-04 23:01:50 +0100

    Document the `CompressionUtils` class a bit, it's pretty done

--- src/main/java/com/woernerj/dragonsdogma/bo/CompressionProgressCallback.java
+++ src/main/java/com/woernerj/dragonsdogma/bo/CompressionProgressCallback.java
@@ -1,6 +1,26 @@
 package com.woernerj.dragonsdogma.bo;
 
+import com.woernerj.dragonsdogma.util.CompressionUtils;
+
+/**
+ * Provides a callback to handle reporting the progress of compression
+ * operations started by {@link CompressionUtils}.
+ * 
+ * @author Jordan Woerner
+ * @version 1.0
+ * @since 2016-06-30
+ */
+@FunctionalInterface
 public interface CompressionProgressCallback {
 
-	public default void update(double perc) { /* Do nothing. */ }
+	/**
+	 * Called when the {@link CompressionUtils#decompress(byte[])} or 
+	 * {@link CompressionUtils#compress(byte[])} methods process a block of 
+	 * bytes.
+	 * 
+	 * @param perc The percentage of the data provided that has been processed 
+	 * as a value between 0 and 1.
+	 * @since 1.0
+	 */
+	public void update(double perc);
 }
--- src/main/java/com/woernerj/dragonsdogma/util/CompressionUtils.java
+++ src/main/java/com/woernerj/dragonsdogma/util/CompressionUtils.java
@@ -2,6 +2,7 @@ package com.woernerj.dragonsdogma.util;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.zip.CRC32;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
@@ -10,19 +11,48 @@ import org.apache.commons.lang3.ArrayUtils;
 
 import com.woernerj.dragonsdogma.bo.CompressionProgressCallback;
 
+/**
+ * A simple wrapper for Java ZIP utilities. Provides a neater way to compress 
+ * and decompress data.
+ * 
+ * @author Jordan Woerner
+ * @version 1.2
+ * @since 2016-07-04
+ */
 public class CompressionUtils {
 
+	/**
+	 * Compresses the specified data using the ZLib compression algorithm.
+	 * 
+	 * @param raw The data to be compressed as an array of bytes.
+	 * @return An array of bytes containing the compressed data.
+	 * @since 1.1
+	 */
 	public static byte[] compress(final byte[] raw) {
-		return compress(raw, new CompressionProgressCallback() { });
+		return compress(raw, p -> { /* Do nothing */ });
 	}
-	public static byte[] compress(final byte[] raw, final CompressionProgressCallback callback) {
+	/**
+	 * Compresses the specified data using the ZLib compression algorithm. 
+	 * Allows the specification of a callback to report the progress of the 
+	 * compression operation. 
+	 * 
+	 * @param raw The data to be compressed as an array of bytes.
+	 * @param callback A callback that will report the progress of the 
+	 * compression operation as an implementation of {@link 
+	 * CompressionProgressCallback}.
+	 * @return An array of bytes containing the compressed data.
+	 * @since 1.0
+	 */
+	public static byte[] compress(final byte[] raw, 
+			final CompressionProgressCallback callback) {
 		if (ArrayUtils.isEmpty(raw)) return new byte[0];
 		
 		Deflater deflater = new Deflater();
 		deflater.setInput(raw);
 		deflater.finish();
 		
-		try (ByteArrayOutputStream out = new ByteArrayOutputStream(raw.length)){
+		try (ByteArrayOutputStream out 
+				= new ByteArrayOutputStream(raw.length)){
 			byte[] buffer = new byte[1024];
 			while (!deflater.finished()) {
 				double perc = (deflater.getBytesRead())/(double)raw.length;
@@ -35,17 +65,38 @@ public class CompressionUtils {
 			return null;
 		}
 	}
-	
+
+	/**
+	 * Decompresses the specified data using the ZLib compression algorithm.
+	 * 
+	 * @param raw The data to be compressed as an array of bytes.
+	 * @return An array of bytes containing the decompresses data.
+	 * @since 1.1
+	 */
 	public static byte[] decompress(final byte[] compressed) {
-		return decompress(compressed, new CompressionProgressCallback() { });
+		return decompress(compressed, p -> { /* Do nothing */ });
 	}
-	public static byte[] decompress(final byte[] compressed, final CompressionProgressCallback callback) {
+	/**
+	 * Decompresses the specified data using the ZLib compression algorithm. 
+	 * Allows the specification of a callback to report the progress of the 
+	 * decompression operation. 
+	 * 
+	 * @param raw The compressed data as an array of bytes.
+	 * @param callback A callback that will report the progress of the 
+	 * decompression operation as an implementation of {@link 
+	 * CompressionProgressCallback}.
+	 * @return An array of bytes containing the raw data.
+	 * @since 1.0
+	 */
+	public static byte[] decompress(final byte[] compressed, 
+			final CompressionProgressCallback callback) {
 		if (ArrayUtils.isEmpty(compressed)) return new byte[0];
 		
 		Inflater inflater = new Inflater();
 		inflater.setInput(compressed);
 		
-		try (ByteArrayOutputStream out = new ByteArrayOutputStream(compressed.length)){
+		try (ByteArrayOutputStream out 
+				= new ByteArrayOutputStream(compressed.length)){
 			byte[] buffer = new byte[1024];
 			while (inflater.getRemaining() > 0) {
 				float perc = inflater.getBytesRead()/(float)compressed.length;
@@ -57,5 +108,18 @@ public class CompressionUtils {
 		} catch (DataFormatException | IOException e) {
 			return null;
 		}
+	}
+	
+	/**
+	 * Calculates the CRC error checking code for the specified data.
+	 * 
+	 * @param data The data to generate a CRC code for as an array of bytes.
+	 * @return The CRC code as a long.
+	 * @since 1.2
+	 */
+	public static long calculateCrc32(byte[] data) {
+		CRC32 crc = new CRC32();
+		crc.update(data);
+		return crc.getValue();
 	}
 }
